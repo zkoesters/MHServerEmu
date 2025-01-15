fbfd137ed1776636ab45f2fe1fb73b3b9db4739f
Zachary Koesters <zachary@koeste.rs>
2025-01-14 19:37:02 -0600

    feature(migrations): refactor

--- src/MHServerEmu.DatabaseAccess/Migrations/1736695246_InitializeDatabase.cs
+++ src/MHServerEmu.DatabaseAccess/Migrations/1736695246_InitializeDatabase.cs
@@ -18,121 +18,53 @@ public class InitializeDatabase : Migration {
 
         Create.Table("Player")
             .WithColumn("DbGuid").AsInt64().Unique().NotNullable().PrimaryKey()
+                .ForeignKey("Account", "Id").OnDelete(Rule.Cascade)
             .WithColumn("ArchiveData").AsBinary()
             .WithColumn("StartTarget").AsInt64()
             .WithColumn("StartTargetRegionOverride").AsInt64()
             .WithColumn("AOIVolume").AsInt32();
-        
-        Create.ForeignKey()
-            .FromTable("Player").ForeignColumn("DbGuid")
-            .ToTable("Account").PrimaryColumn("Id")
-            .OnDelete(Rule.Cascade);
 
         Create.Table("Avatar")
             .WithColumn("DbGuid").AsInt64().Unique().NotNullable().PrimaryKey()
-            .WithColumn("ContainerDbGuid").AsInt64()
+            .WithColumn("ContainerDbGuid").AsInt64().Indexed()
+                .ForeignKey("Player", "DbGuid").OnDelete(Rule.Cascade)
             .WithColumn("InventoryProtoGuid").AsInt64()
             .WithColumn("Slot").AsInt32()
             .WithColumn("EntityProtoGuid").AsInt64()
             .WithColumn("ArchiveData").AsBinary();
-        
-        Create.ForeignKey()
-            .FromTable("Avatar").ForeignColumn("ContainerDbGuid")
-            .ToTable("Player").PrimaryColumn("DbGuid")
-            .OnDelete(Rule.Cascade);
         
         Create.Table("TeamUp")
             .WithColumn("DbGuid").AsInt64().Unique().NotNullable().PrimaryKey()
-            .WithColumn("ContainerDbGuid").AsInt64()
+            .WithColumn("ContainerDbGuid").AsInt64().Indexed()
+                .ForeignKey("Player", "DbGuid").OnDelete(Rule.Cascade)
             .WithColumn("InventoryProtoGuid").AsInt64()
             .WithColumn("Slot").AsInt32()
             .WithColumn("EntityProtoGuid").AsInt64()
             .WithColumn("ArchiveData").AsBinary();
-        
-        Create.ForeignKey()
-            .FromTable("TeamUp").ForeignColumn("ContainerDbGuid")
-            .ToTable("Player").PrimaryColumn("DbGuid")
-            .OnDelete(Rule.Cascade);
         
         Create.Table("Item")
             .WithColumn("DbGuid").AsInt64().Unique().NotNullable().PrimaryKey()
-            .WithColumn("ContainerDbGuid").AsInt64()
+            .WithColumn("ContainerDbGuid").AsInt64().Indexed()
+                .ForeignKey("Player", "DbGuid").OnDelete(Rule.Cascade)
+                .ForeignKey("Avatar", "DbGuid").OnDelete(Rule.Cascade)
+                .ForeignKey("TeamUp", "DbGuid").OnDelete(Rule.Cascade)
             .WithColumn("InventoryProtoGuid").AsInt64()
             .WithColumn("Slot").AsInt32()
             .WithColumn("EntityProtoGuid").AsInt64()
             .WithColumn("ArchiveData").AsBinary();
-        
-        Create.ForeignKey()
-            .FromTable("Item").ForeignColumn("ContainerDbGuid")
-            .ToTable("Player").PrimaryColumn("DbGuid")
-            .OnDelete(Rule.Cascade);
-        
-        Create.ForeignKey()
-            .FromTable("Item").ForeignColumn("ContainerDbGuid")
-            .ToTable("Avatar").PrimaryColumn("DbGuid")
-            .OnDelete(Rule.Cascade);
-        
-        Create.ForeignKey()
-            .FromTable("Item").ForeignColumn("ContainerDbGuid")
-            .ToTable("TeamUp").PrimaryColumn("DbGuid")
-            .OnDelete(Rule.Cascade);
         
         Create.Table("ControlledEntity")
             .WithColumn("DbGuid").AsInt64().Unique().NotNullable().PrimaryKey()
-            .WithColumn("ContainerDbGuid").AsInt64()
+            .WithColumn("ContainerDbGuid").AsInt64().Indexed()
+                .ForeignKey("Avatar", "DbGuid").OnDelete(Rule.Cascade)
             .WithColumn("InventoryProtoGuid").AsInt64()
             .WithColumn("Slot").AsInt32()
             .WithColumn("EntityProtoGuid").AsInt64()
             .WithColumn("ArchiveData").AsBinary();
-        
-        Create.ForeignKey()
-            .FromTable("ControlledEntity").ForeignColumn("ContainerDbGuid")
-            .ToTable("Avatar").PrimaryColumn("DbGuid")
-            .OnDelete(Rule.Cascade);
-
-        Create.Index()
-            .OnTable("Avatar").OnColumn("ContainerDbGuid");
-        
-        Create.Index()
-            .OnTable("TeamUp").OnColumn("ContainerDbGuid");
-        
-        Create.Index()
-            .OnTable("Item").OnColumn("ContainerDbGuid");
-        
-        Create.Index()
-            .OnTable("ControlledEntity").OnColumn("ContainerDbGuid");
     }
 
     public override void Down()
     {
-        Delete.ForeignKey()
-            .FromTable("ControlledEntity").ForeignColumn("ContainerDbGuid")
-            .ToTable("Avatar").PrimaryColumn("DbGuid");
-        
-        Delete.ForeignKey()
-            .FromTable("Item").ForeignColumn("ContainerDbGuid")
-            .ToTable("Player").PrimaryColumn("DbGuid");
-        
-        Delete.ForeignKey()
-            .FromTable("Item").ForeignColumn("ContainerDbGuid")
-            .ToTable("Avatar").PrimaryColumn("DbGuid");
-        
-        Delete.ForeignKey()
-            .FromTable("Item").ForeignColumn("ContainerDbGuid")
-            .ToTable("TeamUp").PrimaryColumn("DbGuid");
-        
-        Delete.ForeignKey()
-            .FromTable("TeamUp").ForeignColumn("ContainerDbGuid")
-            .ToTable("Player").PrimaryColumn("DbGuid");
-        
-        Delete.ForeignKey()
-            .FromTable("Avatar").ForeignColumn("ContainerDbGuid")
-            .ToTable("Player").PrimaryColumn("DbGuid");
-        
-        Delete.ForeignKey()
-            .FromTable("Player").ForeignColumn("DbGuid")
-            .ToTable("Account").PrimaryColumn("Id");
-        
         Delete.Table("ControlledEntity");
         Delete.Table("Item");
         Delete.Table("TeamUp");
